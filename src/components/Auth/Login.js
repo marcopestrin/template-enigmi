945624b993fe5bb1ebb08f9057a46b8a2005c2b2
Marco Pestrin <pestrinmarco@gmail.com>
2018-11-08 07:08:17 +1000

    switch login/registration

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,6 +1,8 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import fire from '../../config/Fire';
+import React, { Component }     from 'react';
+import { Redirect }             from 'react-router-dom';
+import fire                     from '../../config/Fire';
+import Button                   from '../UI/Button/Button';
+import Aux                      from "../../hoc/Aux";
 
 class Login extends Component {
   constructor(props) {
@@ -8,9 +10,10 @@ class Login extends Component {
     this.login = this.login.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.signup = this.signup.bind(this);
+    this.isSignUp = true
     this.state = {
-      email: '',
-      password: ''
+        email: '',
+        password: ''
     };
   }
 
@@ -20,33 +23,71 @@ class Login extends Component {
 
   login(e) {
     e.preventDefault();
-    fire.auth().signInWithEmailAndPassword(this.state.email, this.state.password).then((u)=>{
-    }).catch((error) => {
-        console.log(error);
-      });
+    console.log("isSignUp",this.state.isSignUp);
+      if(this.state.isSignUp) {
+          console.log("REGISTRAZIØNE");
+        fire.auth().createUserWithEmailAndPassword(this.state.email, this.state.password)
+            .then((u)=>{
+                console.log(u)
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+      }else{
+        console.log("ACCEDI");
+
+        fire.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
+            .then((u)=>{
+                console.log(u);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+      }
   }
 
   signup(e){
     e.preventDefault();
-    fire.auth().createUserWithEmailAndPassword(this.state.email, this.state.password).then((u)=>{
-    }).then((u)=>{console.log(u)})
+    fire.auth().createUserWithEmailAndPassword(this.state.email, this.state.password)
+    .then((u)=>{
+        console.log(u)
+    })
     .catch((error) => {
         console.log(error);
       })
   }
+
+  switchAuthModeHandler = (e) => {
+    e.preventDefault();
+      console.log("enttrato in switch");
+    this.setState(prevState => {
+      return {isSignUp:!prevState.isSignUp}
+    });
+  }
+
   render() {
     return (
-        <form>
-            <label for="email1">Email address</label>
-            <input value={this.state.email} onChange={this.handleChange} type="email" name="email1" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-            
-            <label for="password1">Password</label>
-            <input value={this.state.password} onChange={this.handleChange} type="password" name="password" id="password1" placeholder="Password" />
-            
-            <button type="submit" onClick={this.login}>Login</button>
-            <button onClick={this.signup}>Signup</button>
-        </form>
- 
+        <Aux>
+            <h1>{this.state.isSignUp ? 'Registrati' : 'Accedi'}</h1>
+            <form>
+                <label for="email1">Email address</label>
+                <input value={this.state.email} onChange={this.handleChange} type="email" name="email" class="form-control" id="email1" aria-describedby="emailHelp" placeholder="Enter email" />
+                
+                <label for="password1">Password</label>
+                <input value={this.state.password} onChange={this.handleChange} type="password" name="password" id="password1" placeholder="Password" />
+
+                <Button 
+                    clicked={this.login}
+                    btnType="classic">
+                    INVIA MODULO
+                </Button>
+                <Button 
+                    clicked={this.switchAuthModeHandler}
+                    btnType="classic">
+                    SWITCH TO {!this.state.isSignUp ? 'Registrati' : 'Accedi'}
+                </Button>
+            </form>
+        </Aux>
     );
   }
 }
